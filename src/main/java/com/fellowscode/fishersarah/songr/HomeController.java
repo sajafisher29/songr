06a92de2daac797b25bc2dcc255b1cde6148bda9
Sarah Fisher <sajafisher@gmail.com>
2019-10-01 10:04:46 -0700

    Site capitalizes. Working on building css.

--- src/main/java/com/fellowscode/fishersarah/songr/HomeController.java
+++ src/main/java/com/fellowscode/fishersarah/songr/HomeController.java
@@ -1,7 +1,10 @@
 package com.fellowscode.fishersarah.songr;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -11,4 +14,30 @@ public class HomeController {
     public String getHome() {
         return "home";
     }
+
+    //Create a hello world route
+    @GetMapping("/hello")
+    public String getHello(@RequestParam(required = false, defaultValue = "user") String username,
+                           Model model) {
+        System.out.println(username);
+        model.addAttribute("username", username);
+        return "hello";
+    }
+
+    @GetMapping("/hello/{username}")
+    // Username comes from the path
+    public String getHelloRoute(@PathVariable String username, Model model) {
+        model.addAttribute("username", username);
+        return "home";
+    }
+
+    @GetMapping("/capitalize/{text}")
+    // This will return the {text} in capital letters
+    public String getCapitalizeRoute(@PathVariable String text, Model model) {
+        model.addAttribute("text", text.toUpperCase());
+        return "capitalize";
+    }
+
+    //Create a route that turns words into upper case
+
 }
