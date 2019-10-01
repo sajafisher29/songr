df9b21aa351ed834dded555c80e448e3545ecbe5
Sarah Fisher <sajafisher@gmail.com>
2019-10-01 13:33:54 -0700

    Building tests. Constructor test passing.

---
+++ src/test/java/com/fellowscode/fishersarah/songr/AlbumTest.java
@@ -0,0 +1,18 @@
+package com.fellowscode.fishersarah.songr;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlbumTest {
+    Album[] testAlbum;
+
+    //Testing the constructor and toString
+    @Test public void testConstructor() {
+        testAlbum = new Album[1];
+        testAlbum[0] = new Album("Into The Wild", "Eddie Vedder", 16, 1_984, "https://open.spotify.com/album/13xlpKbai3GGoMSr75lOeX");
+            assertEquals("Eddie Vedder's album Into The Wild contains 16 songs and is 1984 seconds for total length. You can find an image of the album here https://open.spotify.com/album/13xlpKbai3GGoMSr75lOeX", "Eddie Vedder's album Into The Wild contains 16 songs and is 1984 seconds for total length. You can find an image of the album here https://open.spotify.com/album/13xlpKbai3GGoMSr75lOeX", testAlbum[0].toString());
+    }
+
+}
